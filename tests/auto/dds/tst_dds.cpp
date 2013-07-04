c6d40afb8b87084fe8f7e50c8e1fdd600317a497
Ivan Komissarov <ABBAPOH@me.com>
2013-07-04 17:39:11 +0400

    Add RGB10A2 test.

--- tests/auto/dds/tst_dds.cpp
+++ tests/auto/dds/tst_dds.cpp
@@ -41,6 +41,7 @@ void DDSTest::readImage_data()
     QTest::newRow("7") << QString("RGBA8") << QSize(64, 64);
     QTest::newRow("8") << QString("RGB565") << QSize(64, 64);
     QTest::newRow("9") << QString("RGBA5551") << QSize(64, 64);
+    QTest::newRow("10") << QString("RGB10A2") << QSize(64, 64);
 }
 
 void DDSTest::readImage()
